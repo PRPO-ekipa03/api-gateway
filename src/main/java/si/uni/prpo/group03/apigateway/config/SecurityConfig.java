87658681ed8c0e6e3c4df271f6220041b0630308
Ruben Ferreira <rf5885@student.uni-lj.si>
2025-01-05 18:36:20 +0100

    CORS, addition of route in venue service

--- src/main/java/si/uni/prpo/group03/apigateway/config/SecurityConfig.java
+++ src/main/java/si/uni/prpo/group03/apigateway/config/SecurityConfig.java
@@ -8,8 +8,12 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import si.uni.prpo.group03.apigateway.security.JwtFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import si.uni.prpo.group03.apigateway.security.JwtFilter;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -21,6 +25,8 @@ public class SecurityConfig {
     public DefaultSecurityFilterChain securityWebFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors() // Enable CORS
+                .and()
                 .sessionManagement(sessionManagement -> sessionManagement
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
@@ -31,6 +37,20 @@ public class SecurityConfig {
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        String frontendHost = System.getenv().getOrDefault("FRONTEND_HOST", "http://localhost:4200");
+        configuration.setAllowedOrigins(List.of(frontendHost)); // Allow specific origin
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allow specific HTTP methods
+        configuration.setAllowedHeaders(List.of("*")); // Allow all headers
+        configuration.setExposedHeaders(List.of("Authorization")); // Allow Authorization header in responses
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Apply CORS config to all endpoints
+        return source;
     }
 
 }
